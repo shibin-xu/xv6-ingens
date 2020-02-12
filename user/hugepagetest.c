532180e71e7ec1aa6c3a8651c7df219a4f91fb42
Jack Xu <shibix@Jacks-MacBook-Air.local>
2020-02-12 11:53:29 -0800

    add huge page test

---
+++ user/hugepagetest.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char **argv)
+{
+  int sz = 4096 * 512 * 10;
+  printf("malloc %d bytes\n", sz);
+  char *arr = malloc(sz);
+
+  printf("sequential access\n", sz);
+  for (int i = 0; i < sz; i++)
+    *(arr + i) = 1;
+
+  printf("free %d bytes\n", sz);
+  free(arr);
+
+  exit(0);
+}
