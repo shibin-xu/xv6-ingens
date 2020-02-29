80f0005d34479069d0bdbda8c33aecc327b5607a
Su Ye <yes23@cs.washington.edu>
2020-02-28 17:56:02 -0800

    smallworkertest: added print msg, tested no memeory leak.

--- user/smallworkertest.c
+++ user/smallworkertest.c
@@ -7,17 +7,27 @@ main(int argc, char **argv)
     int pids[20];
     int sz = 4096 * 400; // not a huge page, not 80%
     int xstatus = 0;
-
-    for(int i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
-        if((pids[i] = fork()) == 0){
+    int i;
+    for(i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
+        pids[i] = fork();
+        if (pids[i]< -1) {
+            printf(" fork %d failled. ERROR\n", i);
+            while(i-- > 0) {
+                kill(pids[i]);
+            }
+            exit(1);
+        }
+        if(pids[i] == 0){
+            
             int *arr = malloc(sz);
             // write once
             for (int j = 0; j < sz / sizeof(int); j++) {
                 *(arr + j) = pids[i];
             }
-
-            // keep on accessing the memory until killed
-            for (int k = 0; k < 1000000; k++) {
+            
+            printf("start work %d.  ", i);
+            //keep on accessing the memory until killed
+            for (int k = 0; k < 300; k++) {
                 for (int j = 0; j < sz / sizeof(int); j++) {
                     if (*(arr + j) != pids[i]) {
                         xstatus = 1;
@@ -25,13 +35,23 @@ main(int argc, char **argv)
                 }   
             }
             free(arr);
+            printf("\ndone work %d\n", i); // almost expect no one is done? 
+            for(;;) sleep(1000);
+        } else {
+            printf("forked %d.  ", i);
         }
     }
 
-    sleep(3); // wait for worker to finish.
-    for(int i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
+    sleep(6); // wait for worker to finish.
+    printf("\nparent wake up\n");
+
+    while(i-- > 0) {
         kill(pids[i]);
     }
+    // for(i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
+    //     printf(" killed %d\n", i);
+    //     kill(pids[i]);
+    // }
 
     if (xstatus)
         printf(" ERROR\n");
